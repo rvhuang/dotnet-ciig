7c506c76a988a1db19132a2291cd78bdff5f14e3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-04 02:44:41 +0000

    Deploy rvhuang/dotnet-ciig to github.com/rvhuang/dotnet-ciig.git:gh-pages

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 // Author: Robert Vandenberg Huang
 // rvh.omni@gmail.com
 var InterfaceInfo = /** @class */ (function () {
-    function InterfaceInfo(typeName, ns, referenceUrl) {
+    function InterfaceInfo(typeName, ns, referenceUrl, shortName) {
         this._typeName = typeName;
         this._namespace = ns;
         this._referenceUrl = referenceUrl;
+        this._shortName = shortName;
     }
     Object.defineProperty(InterfaceInfo.prototype, "typeName", {
         get: function () {
@@ -28,22 +29,29 @@ var InterfaceInfo = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
+    Object.defineProperty(InterfaceInfo.prototype, "shortName", {
+        get: function () {
+            return this._shortName;
+        },
+        enumerable: true,
+        configurable: true
+    });
     InterfaceInfo.createAllInterfaces = function () {
         var interfaces = new Array();
-        interfaces.push(new InterfaceInfo("IEnumerable<T>", "System.Collections.Generic", "system.collections.generic.ienumerable-1"));
-        interfaces.push(new InterfaceInfo("IProducerConsumerCollection<T>", "System.Collections.Concurrent", "system.collections.concurrent.iproducerconsumercollection-1"));
-        interfaces.push(new InterfaceInfo("IReadOnlyCollection<T>", "System.Collections.Generic", "system.collections.generic.ireadonlycollection-1"));
-        interfaces.push(new InterfaceInfo("IReadOnlyList<T>", "System.Collections.Generic", "system.collections.generic.ireadonlylist-1"));
-        interfaces.push(new InterfaceInfo("IReadOnlyDictionary<TKey, TValue>", "System.Collections.Generic", "system.collections.generic.ireadonlydictionary-2"));
-        interfaces.push(new InterfaceInfo("ICollection<T>", "System.Collections.Generic", "system.collections.generic.icollection-1"));
-        interfaces.push(new InterfaceInfo("IList<T>", "System.Collections.Generic", "system.collections.generic.ilist-1"));
-        interfaces.push(new InterfaceInfo("ISet<T>", "System.Collections.Generic", "system.collections.generic.iset-1"));
-        interfaces.push(new InterfaceInfo("IDictionary<TKey, TValue>", "System.Collections.Generic", "system.collections.generic.idictionary-2"));
-        interfaces.push(new InterfaceInfo("IEnumerable<IGrouping<TKey, TElement>>", "System.Linq", "system.linq.igrouping-2"));
-        interfaces.push(new InterfaceInfo("ILookup<TKey, TElement>", "System.Linq", "system.linq.ilookup-2"));
-        interfaces.push(new InterfaceInfo("IOrderedEnumerable<TElement>", "System.Linq", "system.linq.iorderedenumerable-1"));
-        interfaces.push(new InterfaceInfo("IQueryable<T>", "System.Linq", "system.linq.iqueryable-1"));
-        interfaces.push(new InterfaceInfo("IOrderedQueryable<T>", "System.Linq", "system.linq.iorderedqueryable-1"));
+        interfaces.push(new InterfaceInfo("IEnumerable<T>", "System.Collections.Generic", "system.collections.generic.ienumerable-1", "E"));
+        interfaces.push(new InterfaceInfo("IProducerConsumerCollection<T>", "System.Collections.Concurrent", "system.collections.concurrent.iproducerconsumercollection-1", "PCC"));
+        interfaces.push(new InterfaceInfo("IReadOnlyCollection<T>", "System.Collections.Generic", "system.collections.generic.ireadonlycollection-1", "ROC"));
+        interfaces.push(new InterfaceInfo("IReadOnlyList<T>", "System.Collections.Generic", "system.collections.generic.ireadonlylist-1", "ROL"));
+        interfaces.push(new InterfaceInfo("IReadOnlyDictionary<TKey, TValue>", "System.Collections.Generic", "system.collections.generic.ireadonlydictionary-2", "ROD"));
+        interfaces.push(new InterfaceInfo("ICollection<T>", "System.Collections.Generic", "system.collections.generic.icollection-1", "C"));
+        interfaces.push(new InterfaceInfo("IList<T>", "System.Collections.Generic", "system.collections.generic.ilist-1", "L"));
+        interfaces.push(new InterfaceInfo("ISet<T>", "System.Collections.Generic", "system.collections.generic.iset-1", "S"));
+        interfaces.push(new InterfaceInfo("IDictionary<TKey, TValue>", "System.Collections.Generic", "system.collections.generic.idictionary-2", "D"));
+        interfaces.push(new InterfaceInfo("IEnumerable<IGrouping<TKey, TElement>>", "System.Linq", "system.linq.igrouping-2", "G"));
+        interfaces.push(new InterfaceInfo("ILookup<TKey, TElement>", "System.Linq", "system.linq.ilookup-2", "Lkp"));
+        interfaces.push(new InterfaceInfo("IOrderedEnumerable<TElement>", "System.Linq", "system.linq.iorderedenumerable-1", "OE"));
+        interfaces.push(new InterfaceInfo("IQueryable<T>", "System.Linq", "system.linq.iqueryable-1", "Q"));
+        interfaces.push(new InterfaceInfo("IOrderedQueryable<T>", "System.Linq", "system.linq.iorderedqueryable-1", "OQ"));
         return interfaces;
     };
     InterfaceInfo.baseReferenceUrl = "https://docs.microsoft.com/en-us/dotnet/api/";
@@ -83,6 +91,14 @@ var Question = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
+    Question.prototype.hasInterface = function (typeName) {
+        if (this.answer) {
+            return this._interfaces.indexOf(typeName) >= 0; // Whitelisting
+        }
+        else {
+            return this._interfaces.indexOf(typeName) < 0; // Blacklisting
+        }
+    };
     Question.compare = function (a, b) {
         if (a == null && b != null)
             return 1;
@@ -205,7 +221,8 @@ var CheckList = /** @class */ (function () {
         this._questions.forEach(function (q) { return q.yourAnswer = false; });
         this._interfaces = interfaces;
         this._interfaces.sort(function (a, b) { return a.typeName.localeCompare(b.typeName); });
-        this._ticked = new Array();
+        this._tickedQuestions = new Array();
+        this._tickedInterfaces = new Array();
     }
     Object.defineProperty(CheckList.prototype, "allQuestionTexts", {
         get: function () {
@@ -224,11 +241,11 @@ var CheckList = /** @class */ (function () {
     CheckList.prototype.tickQuestion = function (questionText) {
         var _this = this;
         this._questions.filter(function (q) { return q.text == questionText; }).forEach(function (q) { return q.yourAnswer = !q.yourAnswer; });
-        this._ticked = this._questions.filter(function (q) { return q.yourAnswer == q.answer; });
+        this._tickedQuestions = this._questions.filter(function (q) { return q.yourAnswer == q.answer; });
         this._interfaces.sort(function (a, b) {
-            if (_this._ticked.length > 0) {
-                var checkStateA = _this._ticked.every(function (q) { return q.interfaces.indexOf(a.typeName) >= 0; });
-                var checkStateB = _this._ticked.every(function (q) { return q.interfaces.indexOf(b.typeName) >= 0; });
+            if (_this._tickedQuestions.length > 0) {
+                var checkStateA = _this._tickedQuestions.every(function (q) { return q.interfaces.indexOf(a.typeName) >= 0; });
+                var checkStateB = _this._tickedQuestions.every(function (q) { return q.interfaces.indexOf(b.typeName) >= 0; });
                 if (checkStateA && !checkStateB)
                     return -1;
                 if (checkStateB && !checkStateA)
@@ -238,7 +255,34 @@ var CheckList = /** @class */ (function () {
         });
     };
     CheckList.prototype.checkInterfaceAvailability = function (typeName) {
-        return this._ticked != null && this._ticked.length > 0 && this._ticked.every(function (q) { return q.interfaces.indexOf(typeName) >= 0; });
+        return this._tickedQuestions != null && this._tickedQuestions.length > 0 && this._tickedQuestions.every(function (q) { return q.hasInterface(typeName); });
+    };
+    CheckList.prototype.tickInterface = function (typeName) {
+        var _this = this;
+        var tickedInterfaces = this._tickedInterfaces.filter(function (i) { return i.typeName != typeName; });
+        if (tickedInterfaces.length == this._tickedInterfaces.length) {
+            this._interfaces.forEach(function (i) {
+                if (i.typeName == typeName) {
+                    tickedInterfaces.push(i);
+                }
+            });
+        }
+        this._tickedInterfaces = tickedInterfaces;
+        this._questions.sort(function (a, b) {
+            var countA = 0;
+            var countB = 0;
+            _this._tickedInterfaces.forEach(function (i) { countA += a.hasInterface(i.typeName) ? 1 : 0; });
+            _this._tickedInterfaces.forEach(function (i) { countB += b.hasInterface(i.typeName) ? 1 : 0; });
+            if (countA > countB)
+                return -1;
+            if (countA < countB)
+                return 1;
+            return 0;
+        });
+    };
+    CheckList.prototype.checkQuestionAvailability = function (questoinText) {
+        var _this = this;
+        return this._tickedInterfaces.filter(function (i) { return _this._questions.some(function (q) { return q.text == questoinText && q.hasInterface(i.typeName); }); });
     };
     CheckList.initialize = function (questions) {
         return new CheckList(questions.filter(function (q) { return q.questionType == QuestionType.CheckListItem; }).map(function (q) { return new Question(q); }), InterfaceInfo.createAllInterfaces());
